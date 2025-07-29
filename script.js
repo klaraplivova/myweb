b1f74faf7383a846a754191de66c43d5a03fd38a
klaraplivova <126472015+klaraplivova@users.noreply.github.com>
2025-07-29 23:21:37 +0200

    adding responsive window titles

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function loadPage(page) {
     document.getElementById('main-content').src = page;
 }
+
+window.addEventListener('message', function(event) {
+    if (event.data && event.data.type === 'updateTitle') {
+        document.getElementsByClassName('current-tab').textContent = event.data.title;
+    }
+});
